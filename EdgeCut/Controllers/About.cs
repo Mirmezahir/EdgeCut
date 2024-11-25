3de656cb8f51c814103f32ca7be8d1be420bddf4
mirmezahir <mirmazahirmi-bp215@code.edu.az>
2024-11-25 07:58:36 -0800

    EdgeCut

---
+++ EdgeCut/Controllers/About.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EdgeCut.Controllers
+{
+    public class About : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EdgeCut/Controllers/Furnitures.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EdgeCut.Controllers
+{
+    public class Furnitures : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
